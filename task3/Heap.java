f56f28a59e19c7aa5f5f03cfe1be24fe4593591a
sailaja <135030993+sailaja123456789@users.noreply.github.com>
2023-11-15 18:21:19 +0530

    task3 files

--- task3/Heap.java
+++ task3/Heap.java
@@ -1,1 +1,33 @@
+package Sai;
+
+public class Heap {
+    public static void main(String[] args) {
+       
+        Person person1 = new Person("John", 25);
+        Person person2 = new Person("Alice", 30);
+
+   
+        System.out.println("Person 1: " + person1.getName() + ", Age: " + person1.getAge());
+        System.out.println("Person 2: " + person2.getName() + ", Age: " + person2.getAge());
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
 
